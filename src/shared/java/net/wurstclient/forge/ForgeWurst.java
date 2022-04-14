829c53cdc6b02dfd0c81ff618429954fd49abf84
Coby Walters <coby.walters2000@gmail.com>
2022-04-14 10:44:49 -0500

    version update

--- src/shared/java/net/wurstclient/forge/ForgeWurst.java
+++ src/shared/java/net/wurstclient/forge/ForgeWurst.java
@@ -26,7 +26,7 @@ import net.wurstclient.forge.clickgui.ClickGui;
 public final class ForgeWurst {
 	
 	public static final String MODID = "skyblockplus";
-	public static final String VERSION = "0.2.0";
+	public static final String VERSION = "0.2.1";
 	
 	@Instance(MODID)
 	private static ForgeWurst forgeWurst;
