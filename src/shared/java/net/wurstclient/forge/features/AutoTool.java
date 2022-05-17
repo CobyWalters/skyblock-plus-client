df0c2d962730db802b277f41b730b6e9804bf6ce
Coby Walters <coby.walters2000@gmail.com>
2022-05-17 14:48:45 -0500

    overhaul & bug fixes

--- src/shared/java/net/wurstclient/forge/features/AutoTool.java
+++ src/shared/java/net/wurstclient/forge/features/AutoTool.java
@@ -1,6 +1,8 @@
 package net.wurstclient.forge.features;
 
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
@@ -11,12 +13,16 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.wurstclient.fmlevents.WPlayerDamageBlockEvent;
+import net.wurstclient.fmlevents.WUpdateEvent;
 import net.wurstclient.forge.Category;
 import net.wurstclient.forge.Feature;
+import net.wurstclient.forge.ForgeWurst;
 import net.wurstclient.forge.compatibility.WItem;
 import net.wurstclient.forge.compatibility.WMinecraft;
 import net.wurstclient.forge.settings.CheckboxSetting;
 import net.wurstclient.forge.utils.BlockUtils;
+import net.wurstclient.forge.utils.ChatUtils;
+import net.wurstclient.forge.utils.KeyBindingUtils;
 
 public class AutoTool extends Feature {
 	
@@ -36,6 +42,10 @@ public class AutoTool extends Feature {
 							"Won't use tools that are about\n" +
 							 "to break.",
 							false);
+	
+	private int oldSlot;
+	private boolean mining;
+	private long timeOfLastMine;
 	
 	public AutoTool() {
 		super("AutoTool",
@@ -57,32 +67,51 @@ public class AutoTool extends Feature {
 	@Override
 	protected void onDisable() {
 		MinecraftForge.EVENT_BUS.unregister(this);
+		mining = false;
 	}
 	
 	@SubscribeEvent
 	public void onPlayerDamageBlock(WPlayerDamageBlockEvent event) {
 		equipBestTool(event.getPos(), useSwords.isChecked(), useHands.isChecked(), repairMode.isChecked());
+	} 
+	
+	@SubscribeEvent
+	public void onUpdate(WUpdateEvent event) {
+		EntityPlayerSP player = WMinecraft.getPlayer();
+		if (mining && Minecraft.getSystemTime() - timeOfLastMine > 100) {
+			mining = false;
+			player.inventory.currentItem = oldSlot;
+		}
 	}
 	
-	public void equipBestTool(BlockPos pos, boolean useSwords, boolean useHands, 
-							  boolean repairMode) {
-		EntityPlayer player = WMinecraft.getPlayer();
+	
+	public void equipBestTool(BlockPos pos, boolean useSwords, boolean useHands, boolean repairMode) {
+		
+		EntityPlayerSP player = WMinecraft.getPlayer();
 		if (player.capabilities.isCreativeMode)
 			return;
+		AutoEat autoEat = wurst.getFeatures().autoEat;
+		if (autoEat.isEnabled() && autoEat.isEating())
+			return;
+		Killaura killaura = wurst.getFeatures().killaura;
+		if (killaura.isEnabled() && killaura.getTarget(player) != null)
+			return;
 		
+		timeOfLastMine = Minecraft.getSystemTime();
 		InventoryPlayer inventory = player.inventory;
 		IBlockState state = BlockUtils.getState(pos);
-		ItemStack heldItem = player.getHeldItemMainhand();
-		float bestScore = getDestroySpeed(heldItem, state);
-		int fallbackSlot = -1;
-		int bestSlot = -1;
+		float bestScore = 1;
+		int bestSlot = inventory.currentItem;
+
+		if (!mining) {
+			oldSlot = inventory.currentItem;
+			mining = true;
+		}
 		
-		for (int slot = 0; slot < 9; slot++) {
+		for (int i = 0; i < 9; ++i) {
 			
+			int slot = (inventory.currentItem + i) % 9;
 			ItemStack stack = inventory.getStackInSlot(slot);
-			if (fallbackSlot == -1 && !isDamageable(stack))
-				fallbackSlot = slot;
-			
 			float score = getDestroySpeed(stack, state);
 			
 			// apply silk touch and fortune scores
@@ -95,27 +124,29 @@ public class AutoTool extends Feature {
 				score += enchantLevel * .004;
 			}
 			
+			// if the item is a tool, too damaged, and not ready to be repaired, do not switch to it
+			if (isDamageable(stack) && isTooDamaged(stack, repairMode)) {
+				AutoRepair autoRepair = wurst.getFeatures().autoRepair;
+				if (!(autoRepair.isEnabled() && !autoRepair.isSupressed() && autoRepair.canRepair()))
+					score = 0;
+			}
+			
+			// prioritize non-damageabale stacks over damageable stacks
+			if (!isDamageable(stack)) {
+				score += .0001;
+			}
+			
 			if (score <= bestScore)
 				continue;
 			else if (!useSwords && stack.getItem() instanceof ItemSword)
-				continue;
-			else if (isTooDamaged(stack, repairMode))
 				continue;
 			
 			bestScore = score;
 			bestSlot = slot;
 		}
 		
-		boolean useFallback = isDamageable(heldItem) && 
-							  		(isTooDamaged(heldItem, repairMode) ||
-							  		 useHands && getDestroySpeed(heldItem, state) <= 1);
-		
 		if (bestSlot != -1)
 			inventory.currentItem = bestSlot;
-		else if (useFallback && fallbackSlot != -1)
-			inventory.currentItem = fallbackSlot;
-		/*else if (isTooDamaged(heldItem, repairMode))
-			inventory.currentItem = (inventory.currentItem + 1) % 9;*/
 	}
 	
 	private float getDestroySpeed(ItemStack stack, IBlockState state) {
@@ -133,7 +164,8 @@ public class AutoTool extends Feature {
 	}
 	
 	private boolean isTooDamaged(ItemStack stack, boolean repairMode) {
-		return repairMode && stack.getMaxDamage() - stack.getItemDamage() <= 4;
+		double durability = 1 - (double) stack.getItemDamage() / stack.getMaxDamage();
+		return repairMode && durability <= .05;
 	}
 	
 }
